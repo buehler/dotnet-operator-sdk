f4f6fa612f92903ff1f199105fa7493b4ed3c494
Christoph Bühler <christoph@smartive.ch>
2020-09-01 08:59:54 +0200

    feat(testing): split out the testing resources.
    BREAKING CHANGE: This removes the application factory
    from `KubeOps`. Please install the `KubeOps.Testing`
    package if you need the resources.

--- src/KubeOps/Operator/Entities/EntityScope.cs
+++ src/KubeOps/Operator/Entities/EntityScope.cs
@@ -2,7 +2,14 @@
 {
     public enum EntityScope
     {
+        /// <summary>
+        /// The resource is namespace.
+        /// </summary>
         Namespaced,
+
+        /// <summary>
+        /// The resource is cluster-wide.
+        /// </summary>
         Cluster,
     }
 }
