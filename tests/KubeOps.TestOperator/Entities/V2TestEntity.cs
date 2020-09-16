c5cb21f2c5e07d2b8217bc7bf78a07a3aa52b4fd
Christoph Bühler <christoph@smartive.ch>
2020-09-16 17:07:18 +0200

    chore: use windows machine to build docs

--- tests/KubeOps.TestOperator/Entities/V2TestEntity.cs
+++ tests/KubeOps.TestOperator/Entities/V2TestEntity.cs
@@ -10,7 +10,7 @@ namespace KubeOps.TestOperator.Entities
         /// </summary>
         public string Spec { get; set; } = string.Empty;
 
-        public string? Username { get; set; }
+        public string Username { get; set; } = string.Empty;
 
         public IntstrIntOrString StringOrInteger { get; set; } = 42;
     }
