2be368bca5edea55bef3c918e5e2f06acc25bb12
alexmg <alex.meyergleaves@gmail.com>
2021-02-23 01:18:15 +1000

    Minor performance tweak in ToCamelCase extension method

--- src/KubeOps/Operator/Util/StringExtensions.cs
+++ src/KubeOps/Operator/Util/StringExtensions.cs
@@ -3,7 +3,7 @@
     internal static class StringExtensions
     {
         internal static string ToCamelCase(this string value) =>
-            string.IsNullOrWhiteSpace(value)
+            string.IsNullOrWhiteSpace(value) || char.IsLower(value[0])
                 ? value
                 : char.ToLowerInvariant(value[0]) + value[1..];
     }
