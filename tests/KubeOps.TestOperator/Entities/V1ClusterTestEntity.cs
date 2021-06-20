b1000156c61a0d68cf4ef044d90d1172e7b64ca1
Christoph Bühler <cbuehler@rootd.ch>
2021-06-20 19:37:11 +0200

    fix: remove entity scope from kubernetes operator sdk.
    This is provided by Client package.
    
    This fixes #224.

---
+++ tests/KubeOps.TestOperator/Entities/V1ClusterTestEntity.cs
@@ -0,0 +1,25 @@
+﻿using DotnetKubernetesClient.Entities;
+using k8s.Models;
+using KubeOps.Operator.Entities;
+
+namespace KubeOps.TestOperator.Entities
+{
+    public class V1ClusterTestEntitySpec
+    {
+        /// <summary>
+        /// This is a test for the contextual fetching of descriptions.
+        /// </summary>
+        public string Spec { get; set; } = string.Empty;
+    }
+
+    public class V1ClusterTestEntityStatus
+    {
+        public string Status { get; set; } = string.Empty;
+    }
+
+    [KubernetesEntity(Group = "testing.dev", ApiVersion = "v1", Kind = "ClusterTestEntity")]
+    [EntityScope(EntityScope.Cluster)]
+    public class V1ClusterTestEntity : CustomKubernetesEntity<V1ClusterTestEntitySpec, V1ClusterTestEntityStatus>
+    {
+    }
+}
