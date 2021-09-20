0cc5e05fb21f537ed9a48efad4e9fe7afe145eb9
Christoph Bühler <cbuehler@rootd.ch>
2021-09-20 15:14:18 +0200

    feat(CRDs): Allow ShortNames to be configured.
    This closes #271. Adding a custom attribute
    `KubernetesEntityShortNames` which can be used
    to add `shortNames` to a CRD of an entity.
    The attribute needs to be added to the class of
    an entity and is inherited.

---
+++ src/KubeOps/Operator/Entities/Annotations/KubernetesEntityShortNamesAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace KubeOps.Operator.Entities.Annotations
+{
+    /// <summary>
+    /// Define "shortNames" for CRDs.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class KubernetesEntityShortNamesAttribute : Attribute
+    {
+        public KubernetesEntityShortNamesAttribute(params string[] shortNames)
+        {
+            ShortNames = shortNames;
+        }
+
+        /// <summary>
+        /// Array of shortnames that should be attached to CRDs.
+        /// </summary>
+        public string[] ShortNames { get; }
+    }
+}
