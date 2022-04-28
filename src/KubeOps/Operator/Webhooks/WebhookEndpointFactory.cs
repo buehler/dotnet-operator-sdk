d83a1dbf3af9a3c60115e5be0e2de1aaa49e25b5
Mark Lopez <m@silvenga.com>
2022-04-28 00:59:04 -0500

    fix: invalid endpoint construction with K8s primitive entities. (#426)

---
+++ src/KubeOps/Operator/Webhooks/WebhookEndpointFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using DotnetKubernetesClient.Entities;
+
+namespace KubeOps.Operator.Webhooks
+{
+    internal static class WebhookEndpointFactory
+    {
+        internal static string Create<TEntity>(Type owningType, string suffix)
+        {
+            var crd = typeof(TEntity).CreateResourceDefinition();
+
+            var builder = new StringBuilder();
+
+            if (!string.IsNullOrEmpty(crd.Group))
+            {
+                builder.Append($"/{crd.Group}");
+            }
+
+            if (!string.IsNullOrEmpty(crd.Version))
+            {
+                builder.Append($"/{crd.Version}");
+            }
+
+            if (!string.IsNullOrEmpty(crd.Plural))
+            {
+                builder.Append($"/{crd.Plural}");
+            }
+
+            builder.Append($"/{owningType.Name}");
+            builder.Append(suffix);
+
+            return builder.ToString().ToLowerInvariant();
+        }
+    }
+}
