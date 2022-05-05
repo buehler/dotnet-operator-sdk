b6feee2e0ad488f97ee80cdc3081ab6989b53da1
Erin Allison <erin@eallison.us>
2022-05-04 19:06:26 -0500

    Fix spacing
    Signed-off-by: Erin Allison <erin@eallison.us>

--- src/KubeOps/Operator/Caching/IResourceCache.cs
+++ src/KubeOps/Operator/Caching/IResourceCache.cs
@@ -8,8 +8,12 @@ internal interface IResourceCache<TEntity>
     where TEntity : IKubernetesObject<V1ObjectMeta>
 {
     TEntity Get(string id);
+
     TEntity Upsert(TEntity resource, out CacheComparisonResult result);
+
     void Fill(IEnumerable<TEntity> resources);
+
     void Remove(TEntity resource);
+
     void Clear();
 }
