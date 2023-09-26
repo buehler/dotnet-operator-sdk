e17cd8c1aaa5bf4736e9249121adbb3bab8a8187
Christoph Bühler <buehler@users.noreply.github.com>
2023-09-26 14:50:52 +0200

    refactor: Transpilation and conversion of objects (#614)
    BREAKING CHANGE: This removes the entity and rbac conversion
    from the operator package and extracts them into their own
    `KubeOps.Transpiler` package. All static methods there
    are used for transpilation of the entities and rbac attributes.
    Additional logic for Lease, events and such will reside
    in the CLI.

---
+++ src/KubeOps.Transpiler/Entities.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+
+using k8s.Models;
+
+using KubeOps.Abstractions.Entities;
+using KubeOps.Abstractions.Entities.Attributes;
+
+namespace KubeOps.Transpiler;
+
+/// <summary>
+/// Class for the conversion of types to <see cref="EntityMetadata"/> objects.
+/// </summary>
+public static class Entities
+{
+    /// <summary>
+    /// Converts the given type to an <see cref="EntityMetadata"/> object.
+    /// </summary>
+    /// <param name="entityType">The type to convert.</param>
+    /// <returns>The <see cref="EntityMetadata"/> object representing the type with the scope of the entity.</returns>
+    /// <exception cref="ArgumentException">Thrown if the given type is not a valid Kubernetes entity.</exception>
+    public static (EntityMetadata Metadata, string Scope) ToEntityMetadata(Type entityType)
+        => (entityType.GetCustomAttribute<KubernetesEntityAttribute>(),
+                entityType.GetCustomAttribute<EntityScopeAttribute>()) switch
+            {
+                (null, _) => throw new ArgumentException("The given type is not a valid Kubernetes entity."),
+                ({ } attr, var scope) => (new(
+                        Defaulted(attr.Kind, entityType.Name),
+                        Defaulted(attr.ApiVersion, "v1"),
+                        attr.Group,
+                        attr.PluralName),
+                    scope switch
+                    {
+                        null => Enum.GetName(typeof(EntityScope), EntityScope.Namespaced) ?? "namespaced",
+                        _ => Enum.GetName(typeof(EntityScope), scope.Scope) ?? "namespaced",
+                    }),
+            };
+
+    private static string Defaulted(string value, string defaultValue) =>
+        string.IsNullOrWhiteSpace(value) ? defaultValue : value;
+}
