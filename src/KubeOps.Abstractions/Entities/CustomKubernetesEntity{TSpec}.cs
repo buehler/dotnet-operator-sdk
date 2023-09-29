c62619fa4d2996fda991b687606e64b70692d0cc
Christoph Bühler <christoph@smartive.ch>
2023-09-29 14:04:23 +0200

    feat(abstractions): add custom kubernetes entity helper

---
+++ src/KubeOps.Abstractions/Entities/CustomKubernetesEntity{TSpec}.cs
@@ -0,0 +1,17 @@
+﻿using k8s;
+
+namespace KubeOps.Abstractions.Entities;
+
+/// <summary>
+/// Defines a custom kubernetes entity which can be used in finalizers and controllers.
+/// This entity contains a <see cref="Spec"/>, which means in contains specified data.
+/// </summary>
+/// <typeparam name="TSpec">The type of the specified data.</typeparam>
+public abstract class CustomKubernetesEntity<TSpec> : CustomKubernetesEntity, ISpec<TSpec>
+    where TSpec : new()
+{
+    /// <summary>
+    /// Specification of the kubernetes object.
+    /// </summary>
+    public TSpec Spec { get; set; } = new();
+}
