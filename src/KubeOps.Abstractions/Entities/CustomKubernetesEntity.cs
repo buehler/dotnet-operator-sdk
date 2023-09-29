bcd1f52b3286c26e2707619e1508fcdcfc593506
Christoph Bühler <christoph@smartive.ch>
2023-09-29 14:04:23 +0200

    feat(abstractions): add custom kubernetes entity helper

---
+++ src/KubeOps.Abstractions/Entities/CustomKubernetesEntity.cs
@@ -0,0 +1,16 @@
+﻿using k8s;
+using k8s.Models;
+
+namespace KubeOps.Abstractions.Entities;
+
+/// <summary>
+/// Base class for custom Kubernetes entities. The interface <see cref="IKubernetesObject{TMetadata}"/>
+/// can be used on its own, but this class provides convenience initializers.
+/// </summary>
+public abstract class CustomKubernetesEntity : KubernetesObject, IKubernetesObject<V1ObjectMeta>
+{
+    /// <summary>
+    /// The metadata of the kubernetes object.
+    /// </summary>
+    public V1ObjectMeta Metadata { get; set; } = new();
+}
---
+++ src/KubeOps.Abstractions/Entities/CustomKubernetesEntity{TSpec,TStatus}.cs
@@ -0,0 +1,21 @@
+﻿using k8s;
+
+namespace KubeOps.Abstractions.Entities;
+
+/// <summary>
+/// Defines a custom Kubernetes entity.
+/// This entity contains a spec (like <see cref="CustomKubernetesEntity{TSpec}"/>)
+/// and a status (<see cref="Status"/>) which can be updated to reflect the state
+/// of the entity.
+/// </summary>
+/// <typeparam name="TSpec">The type of the specified data.</typeparam>
+/// <typeparam name="TStatus">The type of the status data.</typeparam>
+public abstract class CustomKubernetesEntity<TSpec, TStatus> : CustomKubernetesEntity<TSpec>, IStatus<TStatus>
+    where TSpec : new()
+    where TStatus : new()
+{
+    /// <summary>
+    /// Status object for the entity.
+    /// </summary>
+    public TStatus Status { get; set; } = new();
+}
