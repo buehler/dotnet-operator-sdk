d405617377ac732099ae19f29af66131c09614ba
Jasmin Oster <joster@anexia.com>
2024-03-01 12:51:59 +0100

    docs(abstractions): smaller documentation improvements

--- src/KubeOps.Abstractions/Events/Publisher.cs
+++ src/KubeOps.Abstractions/Events/Publisher.cs
@@ -4,20 +4,23 @@ using k8s.Models;
 namespace KubeOps.Abstractions.Events;
 
 /// <summary>
-/// This injectable delegate publishes events on entities. Events are created in the same
-/// namespace as the provided entity. However, if no namespace is provided (for example in
-/// cluster wide entities), the "default" namespace is used.
-///
-/// The delegate creates a <see cref="Corev1Event"/> if none does exist or updates the
-/// count and last seen timestamp if the same event already fired.
-///
-/// Events have a hex encoded name of a SHA512 hash. For the delegate to update
-/// an event, the entity, reason, message, and type must be the same.
+/// <para>
+/// Publish a new event for <paramref name="entity"/>. Events are created in the same namespace. However,
+/// if no namespace is provided (e.g. for cluster-wide entities), the "default" namespace is used.
+/// </para>
+/// <para>
+/// This effectively creates a new <see cref="Corev1Event"/> or updates the <see cref="Corev1Event.Count"/> and <see cref="Corev1Event.LastTimestamp"/>
+/// of the existing <see cref="Corev1Event"/>.
+/// </para>
 /// </summary>
+/// <remarks>
+/// Events have a hex encoded name of a SHA512 hash. For the delegate to update an event,
+/// <paramref name="entity"/>, <paramref name="reason"/>, <paramref name="message"/> and <paramref name="type"/> must be the same.
+/// </remarks>
 /// <param name="entity">The entity that is involved with the event.</param>
 /// <param name="reason">The reason string. This should be a machine readable reason string.</param>
 /// <param name="message">A human readable string for the event.</param>
-/// <param name="type">The <see cref="EventType"/> of the event (either normal or warning).</param>
+/// <param name="type">The <see cref="EventType"/> of the event.</param>
 /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
 /// <returns>A task that finishes when the event is created or updated.</returns>
 /// <example>
