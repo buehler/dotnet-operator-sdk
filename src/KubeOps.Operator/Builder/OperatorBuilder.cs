89071f549b86372cd74fea975339396fd4b1331c
Jasmin Oster <joster@anexia.com>
2024-03-07 09:59:30 +0100

    fix(operator): use singleton as the lifetime

--- src/KubeOps.Operator/Builder/OperatorBuilder.cs
+++ src/KubeOps.Operator/Builder/OperatorBuilder.cs
@@ -73,14 +73,20 @@ internal class OperatorBuilder : IOperatorBuilder
 
         // Add the default configuration and the client separately. This allows external users to override either
         // just the config (e.g. for integration tests) or to replace the whole client, e.g. with a mock.
-        Services.TryAddSingleton(KubernetesClientConfiguration.BuildDefaultConfig());
-        Services.TryAddTransient<IKubernetesClient>(services =>
-            new KubernetesClient.KubernetesClient(services.GetRequiredService<KubernetesClientConfiguration>()));
-
-        // We also add the k8s.IKubernetes as a transient service, in order to allow to access internal services
+        // We also add the k8s.IKubernetes as a singleton service, in order to allow to access internal services
         // and also external users to make use of it's features that might not be implemented in the adapted client.
-        Services.TryAddTransient<IKubernetes>(services =>
+        //
+        // Due to a memory leak in the Kubernetes client, it is important that the client is registered with
+        // with the same lifetime as the KubernetesClientConfiguration. This is tracked in kubernetes/csharp#1446.
+        // https://github.com/kubernetes-client/csharp/issues/1446
+        //
+        // The missing ability to inject a custom HTTP client and therefore the possibility to use the .AddHttpClient()
+        // functionalities led us choosing Singleton as the lifetime.
+        Services.TryAddSingleton(_ => KubernetesClientConfiguration.BuildDefaultConfig());
+        Services.TryAddSingleton<IKubernetes>(services =>
             new Kubernetes(services.GetRequiredService<KubernetesClientConfiguration>()));
+        Services.TryAddSingleton<IKubernetesClient, KubernetesClient.KubernetesClient>();
+
         Services.TryAddTransient<IEventPublisherFactory, KubeOpsEventPublisherFactory>();
         Services.TryAddTransient<EventPublisher>(
             services => services.GetRequiredService<IEventPublisherFactory>().Create());
