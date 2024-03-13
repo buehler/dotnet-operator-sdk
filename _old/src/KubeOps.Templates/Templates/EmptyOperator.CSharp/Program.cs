ab3d6087bb178baa2c595c39b353d48971e366b9
Christoph Bühler <cbuehler@rootd.ch>
2024-03-13 10:25:48 +0100

    chore: remove old code

--- _old/src/KubeOps.Templates/Templates/EmptyOperator.CSharp/Program.cs
+++
@@ -1,8 +0,0 @@
-using KubeOps.Operator;
-
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddKubernetesOperator();
-
-var app = builder.Build();
-app.UseKubernetesOperator();
-await app.RunOperatorAsync(args);
