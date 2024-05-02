ce7a7b50ef40ba36fa715dc931254fd639364df3
ian.buse <ian.buse@dh2i.com>
2024-05-02 14:42:38 -0700

    feat(test): Add tests for certs and cert service

---
+++ test/KubeOps.Operator.Web.Test/Certificates/CertificateGenerator.Test.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+using FluentAssertions;
+
+namespace KubeOps.Operator.Web.Test.Certificates
+{
+    public class CertificateGeneratorTest : IDisposable
+    {
+        private readonly CertificateGenerator _certificateGenerator = new(Environment.MachineName);
+
+        [Fact]
+        public void Root_Should_Be_Valid()
+        {
+            var (certificate, key) = _certificateGenerator.Root;
+            
+            certificate.Should().NotBeNull();
+            DateTime.Parse(certificate.GetEffectiveDateString()).Should().BeOnOrBefore(DateTime.UtcNow);
+            certificate.Extensions.Any(e => e is X509BasicConstraintsExtension basic && basic.CertificateAuthority).Should().BeTrue();
+            certificate.HasPrivateKey.Should().BeTrue();
+
+            key.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Server_Should_Be_Valid()
+        {
+            var (certificate, key) = _certificateGenerator.Server;
+
+            certificate.Should().NotBeNull();
+            DateTime.Parse(certificate.GetEffectiveDateString()).Should().BeOnOrBefore(DateTime.UtcNow);
+            certificate.Extensions.Any(e => e is X509BasicConstraintsExtension basic && basic.CertificateAuthority).Should().BeFalse();
+            certificate.HasPrivateKey.Should().BeFalse();
+
+            key.Should().NotBeNull();
+        }
+
+        public void Dispose()
+        {
+            _certificateGenerator.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
