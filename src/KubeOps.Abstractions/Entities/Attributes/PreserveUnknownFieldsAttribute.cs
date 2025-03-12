9874ee2b875118e315e262da9266ab0835f0ebaa
Razvan Grama <Razvan.Grama@ayloconsultants.com>
2025-03-12 19:53:59 +0200

    fix: x-kubernetes-preserve-unknown-fields incorrectly applied on List<T>

--- src/KubeOps.Abstractions/Entities/Attributes/PreserveUnknownFieldsAttribute.cs
+++ src/KubeOps.Abstractions/Entities/Attributes/PreserveUnknownFieldsAttribute.cs
@@ -4,5 +4,5 @@
 /// Defines that a property should keep unknown fields
 /// so that kubernetes does not purge additional structures.
 /// </summary>
-[AttributeUsage(AttributeTargets.Property)]
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Class)]
 public class PreserveUnknownFieldsAttribute : Attribute;
