e8ba2e910ddd107938883a46fde75cd7bb477186
Razvan Grama <Razvan.Grama@ayloconsultants.com>
2025-04-15 15:50:45 +0300

    skipping linter error. see comment for details

--- src/KubeOps.Abstractions/Entities/IEntityLabelSelector{TEntity}.cs
+++ src/KubeOps.Abstractions/Entities/IEntityLabelSelector{TEntity}.cs
@@ -3,8 +3,13 @@ using k8s.Models;
 
 namespace KubeOps.Abstractions.Entities;
 
+// This is the same pattern used by Microsoft on ILogger<T>.
+// An alternative would be to use a KeyedSingleton when registering this however that's only valid from .NET 8 and above.
+// Other methods are far less elegant
+#pragma warning disable S2326
 public interface IEntityLabelSelector<TEntity>
     where TEntity : IKubernetesObject<V1ObjectMeta>
 {
     ValueTask<string?> GetLabelSelectorAsync(CancellationToken cancellationToken);
 }
+#pragma warning restore S2326
