a64cc15a380544d55ccca4fb34377e2e2ddf918d
rgrama <rgrama@me.com>
2025-04-28 23:28:44 +0300

    feat: Resource Watcher with Labels (#866)
    This feature extends the ResourceWatcher functionality with support for
    watching only resources which match the label restrictions.
    
    ---------
    
    Co-authored-by: Razvan Grama <Razvan.Grama@ayloconsultants.com>

---
+++ src/KubeOps.Abstractions/Entities/IEntityLabelSelector{TEntity}.cs
@@ -0,0 +1,15 @@
+﻿using k8s;
+using k8s.Models;
+
+namespace KubeOps.Abstractions.Entities;
+
+// This is the same pattern used by Microsoft on ILogger<T>.
+// An alternative would be to use a KeyedSingleton when registering this however that's only valid from .NET 8 and above.
+// Other methods are far less elegant
+#pragma warning disable S2326
+public interface IEntityLabelSelector<TEntity>
+    where TEntity : IKubernetesObject<V1ObjectMeta>
+{
+    ValueTask<string?> GetLabelSelectorAsync(CancellationToken cancellationToken);
+}
+#pragma warning restore S2326
