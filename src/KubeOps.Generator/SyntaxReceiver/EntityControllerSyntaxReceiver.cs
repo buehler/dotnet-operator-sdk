494368686914d8c722e185f73c49c108b2cf924b
Marcus Kimpenhaus <kimpenhaus@devil-engineering.de>
2025-04-28 21:32:42 +0200

    feat: support inheritance for controllers and finalizers (#873)
    @buehler maybe you can double check this PR if you will find some time.
    
    we came accross the issue, that any controller and finalizer need to
    implement `IEntityController<TEntity>` or corresponding
    `IEntityFinalizer<TEntity>` to generate the appropriate entries to
    regiter them in the ServiceCollection. Having as base class like
    EntityControllerBase<TEntity> / EntityFinalizerBase<TEntity> which
    implements `IEntity...` wouldn't work.
    
    As a workaround I just added a redundant `IEntity...` on the inherited
    class as well as on the base class. Not the sweetest solution but a
    working one :)
    
    As far as I could see this was due to the pure textual syntax analysis
    which just checked for the baselist-types. I've changed this to walk the
    semantic syntax model and support inheritance upon multi-levels.
    
    thanks in advance.
    
    ---------
    
    Co-authored-by: Christoph Bühler <buehler@users.noreply.github.com>

--- src/KubeOps.Generator/SyntaxReceiver/EntityControllerSyntaxReceiver.cs
+++ src/KubeOps.Generator/SyntaxReceiver/EntityControllerSyntaxReceiver.cs
@@ -3,20 +3,45 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace KubeOps.Generator.SyntaxReceiver;
 
-internal class EntityControllerSyntaxReceiver : ISyntaxContextReceiver
+internal sealed class EntityControllerSyntaxReceiver : ISyntaxContextReceiver
 {
+    private const string IEntityControllerMetadataName = "KubeOps.Abstractions.Controller.IEntityController`1";
+
     public List<(ClassDeclarationSyntax Controller, string EntityName)> Controllers { get; } = [];
 
     public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
     {
-        if (context.Node is not ClassDeclarationSyntax { BaseList.Types.Count: > 0 } cls ||
-            cls.BaseList.Types.FirstOrDefault(t => t is
-            { Type: GenericNameSyntax { Identifier.Text: "IEntityController" } }) is not { } baseType)
+        if (context.Node is not ClassDeclarationSyntax classDeclarationSyntax)
         {
             return;
         }
 
-        var targetEntity = (baseType.Type as GenericNameSyntax)!.TypeArgumentList.Arguments.First();
-        Controllers.Add((cls, targetEntity.ToString()));
+        if (context.SemanticModel.GetDeclaredSymbol(classDeclarationSyntax) is not INamedTypeSymbol classSymbol)
+        {
+            return;
+        }
+
+        if (classSymbol.IsAbstract)
+        {
+            return;
+        }
+
+        var iEntityControllerInterface = context.SemanticModel.Compilation.GetTypeByMetadataName(IEntityControllerMetadataName);
+        if (iEntityControllerInterface is null)
+        {
+            return;
+        }
+
+        var implementedControllerInterface = classSymbol.AllInterfaces
+            .FirstOrDefault(i => i.IsGenericType && SymbolEqualityComparer.Default.Equals(i.OriginalDefinition, iEntityControllerInterface));
+
+        var entityTypeSymbol = implementedControllerInterface?.TypeArguments.FirstOrDefault();
+
+        if (entityTypeSymbol == null)
+        {
+            return;
+        }
+
+        Controllers.Add((classDeclarationSyntax, entityTypeSymbol.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat)));
     }
 }
--- src/KubeOps.Generator/SyntaxReceiver/EntityFinalizerSyntaxReceiver.cs
+++ src/KubeOps.Generator/SyntaxReceiver/EntityFinalizerSyntaxReceiver.cs
@@ -3,20 +3,45 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace KubeOps.Generator.SyntaxReceiver;
 
-internal class EntityFinalizerSyntaxReceiver : ISyntaxContextReceiver
+internal sealed class EntityFinalizerSyntaxReceiver : ISyntaxContextReceiver
 {
+    private const string IEntityFinalizerMetadataName = "KubeOps.Abstractions.Controller.IEntityFinalizer`1";
+
     public List<(ClassDeclarationSyntax Finalizer, string EntityName)> Finalizer { get; } = [];
 
     public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
     {
-        if (context.Node is not ClassDeclarationSyntax { BaseList.Types.Count: > 0 } cls ||
-            cls.BaseList.Types.FirstOrDefault(t => t is
-            { Type: GenericNameSyntax { Identifier.Text: "IEntityFinalizer" } }) is not { } baseType)
+        if (context.Node is not ClassDeclarationSyntax classDeclarationSyntax)
         {
             return;
         }
 
-        var targetEntity = (baseType.Type as GenericNameSyntax)!.TypeArgumentList.Arguments.First();
-        Finalizer.Add((cls, targetEntity.ToString()));
+        if (context.SemanticModel.GetDeclaredSymbol(classDeclarationSyntax) is not INamedTypeSymbol classSymbol)
+        {
+            return;
+        }
+
+        if (classSymbol.IsAbstract)
+        {
+            return;
+        }
+
+        var iEntityFinalizerInterface = context.SemanticModel.Compilation.GetTypeByMetadataName(IEntityFinalizerMetadataName);
+        if (iEntityFinalizerInterface is null)
+        {
+            return;
+        }
+
+        var implementedEntityFinalizerInterface = classSymbol.AllInterfaces
+            .FirstOrDefault(i => i.IsGenericType && SymbolEqualityComparer.Default.Equals(i.OriginalDefinition, iEntityFinalizerInterface));
+
+        var entityTypeSymbol = implementedEntityFinalizerInterface?.TypeArguments.FirstOrDefault();
+
+        if (entityTypeSymbol == null)
+        {
+            return;
+        }
+
+        Finalizer.Add((classDeclarationSyntax, entityTypeSymbol.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat)));
     }
 }
