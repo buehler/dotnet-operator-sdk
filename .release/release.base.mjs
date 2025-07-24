2783504a69e79b325896aaa88bb81ad2897d1128
Christoph Bühler <christoph.buehler@unisg.ch>
2025-07-24 11:01:02 +0200

    chore: improve dependency version bumps and release notes

--- .release/release.base.mjs
+++ .release/release.base.mjs
@@ -1,8 +1,35 @@
 export default {
   debug: true,
   plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+        releaseRules: [{ type: "deps", scope: "core", release: "patch" }],
+      },
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
+        presetConfig: {
+          types: [
+            { type: "feat", section: "Features" },
+            { type: "fix", section: "Bug Fixes" },
+            { type: "perf", section: "Performance Improvements" },
+            { type: "deps", section: "Dependencies" },
+            { type: "revert", section: "Reverts" },
+            { type: "docs", section: "Documentation" },
+            { type: "style", section: "Styles", hidden: true },
+            { type: "chore", section: "Miscellaneous Chores", hidden: true },
+            { type: "refactor", section: "Code Refactoring", hidden: true },
+            { type: "test", section: "Tests", hidden: true },
+            { type: "build", section: "Build System", hidden: true },
+            { type: "ci", section: "Continuous Integration", hidden: true },
+          ],
+        },
+      },
+    ],
     [
       "semantic-release-net",
       {
